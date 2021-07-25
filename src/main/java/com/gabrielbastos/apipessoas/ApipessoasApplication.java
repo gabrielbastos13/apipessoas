1cdb9a0623fb2d476071f6d7d661caa447574015
Gabriel <gabriel.bastos13@hotmail.com>
2021-07-25 16:57:39 -0300

    first commit

---
+++ src/main/java/com/gabrielbastos/apipessoas/ApipessoasApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbastos.apipessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielbastos/apipessoas/ApipessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbastos.apipessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
